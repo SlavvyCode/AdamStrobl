31c45b9b91a4189af8902819161a58b0b6428fef
vanessabielena <vanessabielena@gmail.com>
2023-03-05 12:48:36 +0100

    Adding a factorial methond in a Factorial.java to the branch.

---
+++ src/main/java/cz/cvut/fel/ts1/Factorial.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.ts1;
+
+public class Factorial {
+    public static int factorialIterative(int n){
+        int result=1,i=1;
+        while(i<=n){
+            result=result*i;
+            i++;
+        }
+
+        return result;
+    }
+}
